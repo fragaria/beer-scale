613929c24478015f3ca352fc310b679a56909d6c
Pavel Vojacek <pavel.vojacek@fragaria.cz>
2019-08-16 17:24:38 +0200

    First try to write to influx

--- serial-influx/serial-influx.js
+++ serial-influx/serial-influx.js
@@ -1,5 +1,7 @@
 const SerialPort = require('serialport');
 const Delimiter = require('@serialport/parser-delimiter');
+const Influx = require('influxdb-nodejs');
+const client = new Influx('http://127.0.0.1:8086/beer');
 
 const port = new SerialPort('/dev/ttyUSB0', {
     baudRate: 38400
@@ -7,13 +9,30 @@ const port = new SerialPort('/dev/ttyUSB0', {
 
 const parser = port.pipe(new Delimiter({ delimiter: '\n' }));
 
+const fieldSchema = {
+    count: 'i',
+    temperature: 'f',
+};
+const tagSchema = {
+};
+client.schema('beer', fieldSchema, tagSchema, {
+    stripUnknown: true,
+});
+
 parser.on('data', (data) => {
-    console.log(data.toString());
     const parsed = /t:(\d+) count:(\d+) temp: (\d+.\d+)/.exec(data);
     if (parsed) {
         const t = parseInt(parsed[1]);
         const beerCount = parseInt(parsed[2], 10);
         const temperature = parseFloat(parsed[3]);
         console.log(t, beerCount, temperature);
+        client.write('beer')
+            .tag({})
+            .field({
+                count: beerCount,
+                temperature: temperature,
+            })
+            .then(() => console.info('write point success'))
+            .catch(console.error);
     }
 });
